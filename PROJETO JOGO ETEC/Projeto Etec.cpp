650a458d650cdb06336b9e99fff7af98bd540e71
GuilhermeFernandes01 <guilherme.fernandes01@outlook.com>
2017-06-01 22:49:10 -0300

    Update Projeto Etec.cpp
    O que foi atualizado?
    Validação do número digitado pelo usuário. Linhas 47 a 58.
    Adição da parte em inglês completa, como a em português (ainda não traduzida)

--- PROJETO JOGO ETEC/Projeto Etec.cpp
+++ PROJETO JOGO ETEC/Projeto Etec.cpp
@@ -6,18 +6,14 @@
 using namespace std;
 
 int main(){
-	//matriz para floresta
-	int mFloresta[10][10];
-	//var para repetiçoes
-	int i,j;
-	//var para utilidades
-	int idioma,arvore=0,tiro=5,tiroArvore;
-	//var para randomico (local alien)
-	int nRandomico,nStatico;
+	
+	int mFloresta[10][10]; //matriz para floresta
+	int i,j; //var para repetiÃ§oes
+	int idioma,arvore=0,tiro=5,tiroArvore; //var para utilidades
+	int nRandomico,nStatico; //var para randomico (local alien)
 	
 	
 	srand(time(0));
-		
 	nRandomico=rand()%100;
 	nStatico=nRandomico;
 	
@@ -48,15 +44,17 @@ int main(){
 			cout<<"\n\nEscolha uma arvore para atirar\n\n>>> "; cin>>tiroArvore;
 			cout<<endl<<endl;
 			
-			if(tiroArvore==nStatico){
+			if(tiroArvore==nStatico && tiroArvore>=1){
 				tiro=tiro-10;
 				cout<<"\nTIROS RESTANTES: "<<tiro+10<<endl;
 				cout<<"PARABENS VOCE ELIMINOU O MARCIANO!\n\n";
-			}else if(tiroArvore!=nStatico){
+			}else if(tiroArvore!=nStatico && tiroArvore>=1){
 				cout<<endl;
 
 				cout<<"\nVoce errou!\n\n";
 				system("pause");
+			}else{
+				cout << "Voce perdeu um tiro por atirar em uma arvore que nao existe!\n\n";
 			}
 			if(tiro==0){
 				cout<<"\n	TIROS: "<<tiro<<endl;
@@ -65,14 +63,13 @@ int main(){
 			}
 		}
 		cout<<endl<<endl;
-	
 	}else if(idioma==2){
 		cout<<"\n                            WELCOME TO THE JUNGLE"<<endl<<endl<<endl;
 		cout<<"\n Situacao:"<<endl<<endl;
 		cout<<"	Voce e um lenhador, um dia voce estava passeando pela floresta de 100 arvores e de repente ele avistou uma luz estranha no ceu, se aproximou da luz e percebeu que desceu algo da luz, um marciano se escondeu atras de uma das 100 arvores, assustado, sacou sua espingarda que tem apenas 5 tiros";
 		cout<<"\nAgora voce esta na floresta e esta tentando acertar o marciano.";
 		cout<<"\n\n                                    FLOREST"<<endl<<endl;
-		while(tiro>0){
+				while(tiro>0){
 			cout<<"\n	TIROS: "<<tiro;
 			cout<<endl<<endl;
 			for(i=0;i<=9;i++){
@@ -81,13 +78,32 @@ int main(){
 					cout<<arvore<<" ";
 				}
 			}
-			system("pause");
+			cout<<endl<<endl;
 			tiro--;
+			arvore=0;
+			
+			cout<<"\n\nEscolha uma arvore para atirar\n\n>>> "; cin>>tiroArvore;
+			cout<<endl<<endl;
+			
+			if(tiroArvore==nStatico && tiroArvore>=1){
+				tiro=tiro-10;
+				cout<<"\nTIROS RESTANTES: "<<tiro+10<<endl;
+				cout<<"PARABENS VOCE ELIMINOU O MARCIANO!\n\n";
+			}else if(tiroArvore!=nStatico && tiroArvore>=1){
+				cout<<endl;
+
+				cout<<"\nVoce errou!\n\n";
+				system("pause");
+			}else{
+				cout << "Voce perdeu um tiro por atirar em uma arvore que nao existe!\n\n";
+			}
+			if(tiro==0){
+				cout<<"\n	TIROS: "<<tiro<<endl;
+				cout<<"\nSuas balas acabaram!\nVoce foi pego pelo Marciano!\nVoce foi abduzido!!!";
+				cout<<"\nO Marciano estava na arvore "<<nStatico<<endl<<endl;
+			}
 		}
-		cout<<"\n	TIROS: "<<tiro<<endl;
-		cout<<"\n		Suas balas acabaram!\n		Voce foi pego pelo Marciano!\n\n		VC FOI ESTUPRADOOOOO!";
 		cout<<endl<<endl;
-		
 	}else{
 		cout<<"Idioma invalido"<<endl;
 	}
