8421c1dd6c74f4cdbfdee4261010c23e1e14bff7
helamanspadari <helamanspa@gmail.com>
2017-06-02 01:49:27 -0300

    Sobre o projeto (enunciado)

---
+++ Enunciado Projeto.cpp
@@ -0,0 +1,32 @@
+/* 	TRABALHO PARA DIA 29/05/2017
+
+Um lenhador estava passeando pela floresta de 100 arvores e de repente ele avistou uma luz estranha no ceu,
+ele se aproximou da luz e percebeu que algo desceu da luz, um marciano se escondeu atras de uma das 100 arvores,
+o caçador sacou a espingarda que tem apenas 5 tiros e tentou acertar o marciano.
+
+E1P em que o caçador com apenas 5 tiros tentara acertar o marciano.
+OBS: caso ele erre, de a dica para ele,
+se esta mais acima;
+se esta mais abaixo;
+se esta mais a direita;
+se esta mais a esquerda;
+
+Se n acertar o marciano dira: VC FOI ESTUPRADO!
+Dê opçao para o jogador, caso ele queira começar a jogar novamente.
+
+--> BASICO  - R
+--> INTERFACE GRAFICA  - B 
+		MODO D.O.S
+--> INTERFACE GRAFICA  - MB
+		MODO WINDOWS
+		
+		
+LP   - logica
+TPI  - grafico
+AS   - documentação
+ING  - opção traduzida
+OSA  - padronização (manual)
+IMC  - requisito minimos
+GSO1 - instalação por diretorio
+LTT  - normatização
+TLBD - banco para aproximação
